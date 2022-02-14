4874907bf376f8cfa1e6750ad9d2e7655c113096
X'Zaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2022-02-14 07:51:26 -0600

    Notes and excersies

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+# crtl + / comments out selected line
+# print("Hello " + input("What is your name?") + "!")
+
+
+# Write a program that prints the number of characters in a user's name.
+# You might need to Google for a function that calculates the length of a string.
+# Ex input Angela
+#     output 6
+# Answer print( len( input("What is your name?") ) )
+
+# Python variables
+# name = input("What is your name?")
+
+# print(name)
+
+# name = "Angela"
+
+# name = input("What is your name?")
+# length = len(name)
+
+# print(length)
