9f7f38c9b6af773fd0dfaabebdee615fcb2248ee
X'Zaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2022-02-15 07:32:44 -0600

    Update with Day 2

--- Day 2/notes.py
+++ Day 2/notes.py
@@ -26,7 +26,7 @@ False
 # # Type Error, Type Checking and Type Conversion
 
 # num_char = len(input("What is your name?"))
-type()  # check type of variable and returns its type
+# type()  # check type of variable and returns its type
 
 # type conversion or also known as type casting
 
@@ -36,3 +36,45 @@ type()  # check type of variable and returns its type
 
 a = float(123)
 print(type(a))
+
+
+# Mathematical Operations
+
+3 + 5
+
+7 - 4
+
+3 * 2
+
+6 / 3  # always a float
+
+2 ** 3  # get the exponent of 2 to the power of 3
+
+# be careful of order of operations
+# PEMDASLR
+# ()
+# **
+# * /
+# + -
+
+# Number Manipulation and F Strings in Python
+
+# print(round(8 / 3, 2)) # round(args, decimal place)
+
+print(8 // 3)  # floor type is integer
+
+result = 4 / 2
+result /= 2
+print(result)
+
+score = 0
+height = 1.85
+isWinning = True
+# User scores a point
+score += 1
+print("your score is " + str(score))
+
+# f-string
+
+print(
+    f"your score is {score}, your height is {height}, your are winning is {isWinning}")
