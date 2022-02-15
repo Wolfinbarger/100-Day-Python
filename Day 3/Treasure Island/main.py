0ce6e62a5f873e2d6773087f4208b191180cc103
X'Zaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2022-02-15 17:49:24 -0600

    Notes for day

---
+++ Day 3/Treasure Island/main.py
@@ -0,0 +1,26 @@
+# Control Flow with if/else and Conditional Operators
+
+# if condition:
+#   do x
+# else:
+#   do y
+
+# comparison Operators
+
+# Operator    Condition
+#    >       Greater than
+#    <         less than
+#    >=   Equal to or greater than
+#    <=     Equal to or less than
+#    ==           Equal
+#    !=           Not
+
+# Nested if statements and elif statements
+
+# if condition:
+#    if condition:
+#       do w
+#   else condition:
+#       do z
+# else:
+#   do y
