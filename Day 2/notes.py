b8d37bc27cc21315ac9fc172dbd6bf3e93cd6ced
X'Zaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2022-02-14 19:53:28 -0600

    Organize folders by Day and added note for Day 2

---
+++ Day 2/notes.py
@@ -0,0 +1,38 @@
+# # Data Types
+
+# String
+
+"Hello"[4]
+
+# Subscripting is pulling out parts of a string
+
+# print("123" + "345")  # cocatinates
+
+
+# Integer - whole numbers, no decimals
+
+# print(123 + 345)
+
+123_456_789
+
+# Float - has decimal place(s)
+
+# Boolean - only two values, True or False
+
+True
+
+False
+
+# # Type Error, Type Checking and Type Conversion
+
+# num_char = len(input("What is your name?"))
+type()  # check type of variable and returns its type
+
+# type conversion or also known as type casting
+
+# new_num_char = str(num_char)
+
+# print("Your name has " + new_num_char + " characters.")
+
+a = float(123)
+print(type(a))
