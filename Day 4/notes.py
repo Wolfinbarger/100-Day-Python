68b9797f85babbd6c9ef9a968a53ca9732cc5608
X'Zaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2022-02-16 19:22:20 -0600

    P1

---
+++ Day 4/notes.py
@@ -0,0 +1,25 @@
+# Randomisation and Python List
+
+# Psuedo randomiser - python uses  Mersenne Twister
+
+# first import
+
+import random
+
+random_integer = random.randint(1, 10)
+
+# Module
+# seperate file use to build final product
+
+random_float = random.random()
+print(random_float)
+
+random_float * 5
+
+# expands range but not include 5
+
+love_score = random.randint(1, 100)
+
+print(f'Your love score is {love_score}')
+
+# Lists
