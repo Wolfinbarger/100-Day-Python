36098b787c0b43325a7ca5099670f8b95e4a2484
X'Zaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2022-02-16 20:59:23 -0600

    Update final day 4

--- Day 4/notes.py
+++ Day 4/notes.py
@@ -4,22 +4,40 @@
 
 # first import
 
-import random
+# import random
 
-random_integer = random.randint(1, 10)
+# random_integer = random.randint(1, 10)
 
 # Module
-# seperate file use to build final product
+# separate file use to build final product
 
-random_float = random.random()
-print(random_float)
+# random_float = random.random()
+# print(random_float)
 
-random_float * 5
+# random_float * 5
 
 # expands range but not include 5
 
-love_score = random.randint(1, 100)
+# love_score = random.randint(1, 100)
 
-print(f'Your love score is {love_score}')
+# print(f'Your love score is {love_score}')
 
 # Lists
+# always have brackets with items seperated by ','
+# fruits = ["Cherry", "Apple", "Pear"]
+
+states_of_america = ["Delware", "Pennsylvania", "Texas"]
+# order of stored data is not lost
+
+states_of_america["Texas"]
+# how to get item from list
+
+# negative index
+states_of_america[-1]
+# counts from end of list
+
+# change items in list by
+states_of_america[1] = "Pencilvania"
+
+# add to end
+states_of_america.append("Angelaland")
