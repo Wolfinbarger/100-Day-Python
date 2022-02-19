55c6419d6dfb2cfa092170a881c4e4e78d679780
X'Zaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2022-02-18 23:48:51 -0600

    add notes

---
+++ Day 6/notes.py
@@ -0,0 +1,17 @@
+# Python Functions & Karel
+
+# functions
+
+# print()
+
+# to create own function
+
+# keywoard: def <name> (parameters):
+# def my_function():
+#   print("hello")
+#   print("bye")
+
+# nothing happens till you execute it
+# to call it
+
+# my_function()
