35885be3df027e8d92ba829ff8aaa3d0bbd1f655
X'Zaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2022-02-18 21:10:07 -0600

    Day note day

---
+++ Day 5/notes.py
@@ -0,0 +1,10 @@
+#
+
+fruits = ["Apple", "Peach", "Pear"]
+
+for fruit in fruits:
+    print(fruit)
+
+    # taking list and assigning fruit to all elements
+
+    # range() args can be start, stop, step
