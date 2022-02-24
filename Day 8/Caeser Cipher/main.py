169c81d77348948fa42d1e9665a10bce20f90e6f
X'Zaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2022-02-23 20:48:58 -0600

    Day 8

--- Day 8/Caeser Cipher/main.py
+++ Day 8/Caeser Cipher/main.py
@@ -4,15 +4,6 @@ from decrypt import decrypt
 from caeser import caeser
 
 
-print(title_caeser_cipher + "\n\n")
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-
-text = input("Type your message:\n").lower()
-
-shift = int(input("Type the shift number:\n"))
-
-
 # if direction == "encode":
 
 #     encrypt(plain_text=text, shift_amount=shift)
@@ -21,4 +12,17 @@ shift = int(input("Type the shift number:\n"))
 
 #     decrypt(cipher_text=text, shift_amount=shift)
 
-caeser(text, shift, direction)
+restart = ""
+
+while restart != 'no':
+    print(title_caeser_cipher + "\n\n")
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+    text = input("Type your message:\n").lower()
+
+    shift = int(input("Type the shift number:\n"))
+
+    caeser(text, shift, direction)
+
+    restart = input("Do you wish to restart?: 'Yes' or 'No'\n").lower()
