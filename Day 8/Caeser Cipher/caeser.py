954524cbb2ba96ca66296de40bd7ecce24d73b32
X'Zaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2022-02-23 20:46:18 -0600

    update to add non latin letters

--- Day 8/Caeser Cipher/caeser.py
+++ Day 8/Caeser Cipher/caeser.py
@@ -10,14 +10,17 @@ def caeser(start_text, shift_amount, cipher_direction):
 
     for letter in start_text:
 
-        if letter is ' ':
+        if letter in alphabet:
+
+            position = alphabet.index(letter)
+
+            new_position = position + shift_amount
+
+            end_text += alphabet[new_position]
+        else:
+
             cipher_text += letter
+
             continue
 
-        position = alphabet.index(letter)
-
-        new_position = position + shift_amount
-
-        end_text += alphabet[new_position]
-
-    print("The {cipher_direction}d text is {end_text}")
+    print(f"The {cipher_direction}d text is {end_text}")
