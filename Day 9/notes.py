4ab9bb5b33638f8633edd62fdc8c5b08474db53c
X'Zaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2022-02-28 21:40:58 -0600

    Started Day 9 notes

---
+++ Day 9/notes.py
@@ -0,0 +1,26 @@
+# Dictionaries
+
+# Dictionaries nesting
+
+# Dictionaries
+# Key              value
+# bug              an error in a program that prevents the program from running as expected
+
+{Key: Value}
+
+programming_dictionary = {
+    "Bug": "An error in a program from rnning as expected",
+    "Function": "A piece of code that you can easily call over and over again",
+}
+
+
+print(programming_dictionary["Bug"])
+
+# if key does not exist get key error
+
+# Adding new entries to dictionary
+programming_dictionary["Loop"] = "the action of doing something over and over again"
+
+"Wipe dictionary"
+programming_dictionary = {}
+print(programming_dictionary)
