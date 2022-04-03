593fea0d9f21c3d9151767ddd215ef7f3ffc5afc
X'Zaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2022-04-02 20:39:20 -0500

    add notes

---
+++ Day 16/main.py
@@ -0,0 +1,24 @@
+
+# import another_module
+
+# print(another_module.another_variable)
+
+
+from turtle import Screen, Turtle
+import turtle
+
+
+timmy = Turtle()
+
+print(timmy)
+
+timmy.shape("turtle")
+timmy.color("green")
+timmy.forward(100)
+
+
+my_screen = Screen()
+
+print(my_screen.canvheight)
+print(my_screen.canvwidth)
+my_screen.exitonclick()
---
+++ Day 16/notes.py
@@ -0,0 +1,23 @@
+# How to use OOP
+
+# object
+# attributes
+# methods
+
+# Constructing Objects
+
+# Class
+# example car
+
+# object
+#car = CarBlueprint()
+
+# object attributes
+#speed = 25
+#fuel = 0
+
+# car.speed
+
+
+# methods
+car.stop()
